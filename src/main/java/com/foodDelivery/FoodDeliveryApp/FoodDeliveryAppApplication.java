7ebd5326e2fea2c6b271e137734d3a64900e8c33
Amanyadav9876 <yadavamanindra123@gmail.com>
2026-06-14 22:32:00 +0530

    Enable asynchronous processing in application

--- src/main/java/com/foodDelivery/FoodDeliveryApp/FoodDeliveryAppApplication.java
+++ src/main/java/com/foodDelivery/FoodDeliveryApp/FoodDeliveryAppApplication.java
@@ -2,12 +2,12 @@ package com.foodDelivery.FoodDeliveryApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class FoodDeliveryAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FoodDeliveryAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryAppApplication.class, args);
+    }
 }
